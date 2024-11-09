d9c46494b1814e23f1594e1b6a96980ea66b1089
Emma <emmamartinezgarcia2806@gmail.com>
2024-11-09 21:42:22 +0100

    about completely responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import './navbarStyle.css';
-import image2 from "../../assets/images/image2.png"
+// import image2 from "../../assets/images/image2.png"
 
-function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+// function Navbar() {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
 
-  useEffect(() => {
-    // Añadir o quitar la clase que controla el overflow del body
-    if (isOpen) {
-      document.body.style.overflow = 'hidden'; // Evita que el fondo se desplace
-    } else {
-      document.body.style.overflow = 'auto'; // Permite el desplazamiento normal
-    }
-  }, [isOpen]);
+//   useEffect(() => {
+//     if (isOpen) {
+//       document.body.style.overflow = 'hidden'; 
+//     } else {
+//       document.body.style.overflow = 'auto'; 
+//     }
+//   }, [isOpen]);
 
   
+//   return (
+//     <nav className={`navbar navbar-expand-lg custom-navbar ${isOpen ? 'open' : ''}`}>
+//       <div className="container">
+//         <Link to="/" className="navbar-brand text-white">
+          
+//           <img src={image2} alt="logo image" style={{width:"18%"}}/>
+          
+//         </Link>
+
+//         <button className="navbar-toggler text-white" type="button" onClick={toggleMenu}>
+//           Menu
+//         </button>
+
+//         <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+//           <ul className="navbar-nav ms-auto">
+//             <li className="nav-item">
+//               <Link to="/projects" className="nav-link text-white">
+//                 Projects
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/about" className="nav-link text-white">
+//                 About
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/contact" className="nav-link text-white">
+//                 Contact
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+import { NavLink } from "react-router-dom";
+import image2 from "../../assets/images/image2.png"
+import './navbarStyle.css';
+
+function Navbar() {
+  const handleNavLinkClick = () => {
+    const navCollapse = document.getElementById("navbarNav");
+    if (navCollapse.classList.contains("show")) {
+      navCollapse.classList.remove("show"); 
+    }
+  };
+
   return (
-    <nav className={`navbar navbar-expand-lg custom-navbar ${isOpen ? 'open' : ''}`}>
-      <div className="container">
-        <Link to="/" className="navbar-brand text-white">
-          
-          <img src={image2} alt="logo image" style={{width:"18%"}}/>
-          
-        </Link>
-
-        <button className="navbar-toggler text-white" type="button" onClick={toggleMenu}>
-          Menu
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/" onClick={handleNavLinkClick}>
+        <img src={image2} alt="logo image" style={{width:"18%"}}/>
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-
-        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
-          <ul className="navbar-nav ms-auto">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <Link to="/projects" className="nav-link text-white">
-                Projects
-              </Link>
+              <NavLink
+                className="nav-link"
+                to="/projects"
+                onClick={handleNavLinkClick}
+              >
+                {" "}
+                projects{" "}
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/about" className="nav-link text-white">
-                About
-              </Link>
+              <NavLink
+                className="nav-link"
+                aria-current="page"
+                to="/about"
+                onClick={handleNavLinkClick}
+              >
+                {" "}
+                about{" "}
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/contact" className="nav-link text-white">
-                Contact
-              </Link>
+              <NavLink
+                className="nav-link"
+                to="/contact"
+                onClick={handleNavLinkClick}
+              >
+                {" "}
+                contact{" "}
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -59,4 +129,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
