b123406bd43bb05fe0ed6369e6ffa70a7d491539
Emma <emmamartinezgarcia2806@gmail.com>
2025-07-14 17:56:48 +0200

    Portfolio - changed year and technologies

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,51 +1,59 @@
-
-import React from 'react';
-import { motion } from 'framer-motion';
-import './aboutStyle.css';
+import React from "react";
+import { motion } from "framer-motion";
+import "./aboutStyle.css";
 import yo from "../../assets/images/yo.jpeg";
 
 function About() {
   return (
-    <motion.div 
-      className="about-section"
+    <motion.div
+      className='about-section'
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
       <motion.div
-        className="about-image"
+        className='about-image'
         initial={{ x: -100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.8 }}
       >
-        <img src={yo} alt="image about me" />
+        <img src={yo} alt='image about me' />
       </motion.div>
 
-      <motion.div 
-        className="about-text"
+      <motion.div
+        className='about-text'
         initial={{ x: 100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.8, delay: 0.3 }}
       >
         <section>
           <p>
-          Hi! I’m <strong>Emma</strong>, a 22-year-old Full-Stack Developer with a deep passion for technology and a solid foundation in web development. I recently completed an intensive Full-Stack Web Development bootcamp at Ironhack, where I gained hands-on experience in HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. Through this experience, I formed a developer’s mindset: solving problems, adapting quickly, and tackling new challenges head-on.
+            Hi! I’m <strong>Emma</strong>, a 23-year-old Full-Stack Developer
+            passionate about technology and software development. I have
+            hands-on experience with a variety of technologies including HTML,
+            CSS, Typecript, JavaScript, React, Node.js, Express, and MongoDB.
+            Currently, I am pursuing a degree in Computer Engineering and while
+            working as a Junior Software Developer at a start-up. My key roles
+            include creating AI conversational agents, client integration and
+            maintenance using both code and no-code tools, and specializing in
+            platforms like Twilio and Meta for Developers. I also focus on
+            integrating AI to enhance productivity and am skilled in automation.
           </p>
-        </section>
-        <section>
-          <p>
-          Though early in my professional journey, I’m committed to continuous learning and always building on my skills. I’m a fast learner and thrive on discovering new technologies and frameworks to deepen my knowledge and deliver efficient, impactful solutions. Technology excites me, and I find it incredibly rewarding to solve complex problems using the best tools to make development smooth and scalable.
-          </p>
-        </section>
-        <section>
-          <p>
-          One of my strengths is my enthusiasm for collaboration and growth. I believe in the power of teamwork; working with others not only strengthens the final product but also accelerates my own learning. I’m looking for opportunities in dynamic, creative environments where I can contribute, grow, and make a positive impact.
-          </p>
-        </section>
-        <section>
-          <p>
-          Whether remote or in person, I’m open to relocating if the right opportunity comes along. I look forward to joining a team that values innovation and growth as much as I do!
-          </p>
+          <section>
+            <p>
+              Committed to continuous learning, I thrive on discovering new
+              technologies to deliver efficient solutions. I enjoy solving
+              complex problems and believe in the power of teamwork to
+              strengthen products and accelerate learning.
+            </p>
+          </section>
+          <section>
+            <p>
+              I’m looking for opportunities in dynamic environments where I can
+              contribute, grow, and make a positive impact. I am excited to join
+              a team that values innovation and growth.
+            </p>
+          </section>
         </section>
       </motion.div>
     </motion.div>
